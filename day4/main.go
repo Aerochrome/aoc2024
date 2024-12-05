b4e62b031574843ee551c071f331afa7b6f17a80
Philipp Lätsch <p.laetsch@outlook.de>
2024-12-05 02:01:13 +0100

    day 4 task 2

--- day4/main.go
+++ day4/main.go
@@ -9,6 +9,14 @@ import (
 
 func main() {
 	input := getInput()
+	task1 := task1(input)
+	task2 := task2(input)
+
+	fmt.Printf("task1: %v\n", task1)
+	fmt.Printf("task2: %v\n", task2)
+}
+
+func task1(input []string) int {
 	occuring := 0
 
 	for i, line := range input {
@@ -137,7 +145,136 @@ func main() {
 		}
 	}
 
-	fmt.Printf("occurs: %d", occuring)
+	return occuring
+}
+
+type storeKey struct {
+	x int
+	y int
+}
+
+func task2(input []string) int {
+	aStore := make(map[storeKey]int)
+
+	for i, line := range input {
+		for j, char := range line {
+			if char != 'M' {
+				continue
+			}
+
+			upPossible := false
+			downPossible := false
+
+			if i >= 2 {
+				upPossible = true
+			}
+
+			if len(input)-1-i >= 2 {
+				downPossible = true
+			}
+
+			if j >= 2 {
+				// check i and then upper left
+				if upPossible {
+					byteSlice := make([]byte, 0, 3)
+
+					hi := j
+					vi := i
+
+					for k := 0; k < 3; k++ {
+						byteSlice = append(byteSlice, input[vi][hi])
+
+						hi--
+						vi--
+					}
+
+					if checkForwardsMas(string(byteSlice)) {
+						increaseStore(aStore, j-1, i-1)
+					}
+				}
+
+				// check i and then lower left
+				if downPossible {
+					byteSlice := make([]byte, 0, 3)
+
+					hi := j
+					vi := i
+
+					for k := 0; k < 3; k++ {
+						byteSlice = append(byteSlice, input[vi][hi])
+
+						hi--
+						vi++
+					}
+
+					if checkForwardsMas(string(byteSlice)) {
+						increaseStore(aStore, j-1, i+1)
+					}
+				}
+			}
+
+			if len(line)-1-j >= 2 {
+				// check i and then upper right
+				if upPossible {
+					byteSlice := make([]byte, 0, 3)
+
+					hi := j
+					vi := i
+
+					for k := 0; k < 3; k++ {
+						byteSlice = append(byteSlice, input[vi][hi])
+
+						hi++
+						vi--
+					}
+
+					if checkForwardsMas(string(byteSlice)) {
+						increaseStore(aStore, j+1, i-1)
+					}
+				}
+
+				// check i and then lower right
+				if downPossible {
+					byteSlice := make([]byte, 0, 3)
+
+					hi := j
+					vi := i
+
+					for k := 0; k < 3; k++ {
+						byteSlice = append(byteSlice, input[vi][hi])
+
+						hi++
+						vi++
+					}
+
+					if checkForwardsMas(string(byteSlice)) {
+						increaseStore(aStore, j+1, i+1)
+					}
+				}
+			}
+		}
+	}
+
+	occurring := 0
+
+	for _, el := range aStore {
+		if el > 1 {
+			occurring++
+		}
+	}
+
+	return occurring
+}
+
+func increaseStore(store map[storeKey]int, x, y int) {
+	key := storeKey{x, y}
+
+	if _, ok := store[key]; !ok {
+		store[key] = 1
+		return
+	}
+
+	store[key] = store[key] + 1
 }
 
 func checkBackwards(str string) bool {
@@ -146,6 +283,10 @@ func checkBackwards(str string) bool {
 
 func checkForwards(str string) bool {
 	return str == "XMAS"
+}
+
+func checkForwardsMas(str string) bool {
+	return str == "MAS"
 }
 
 func getInput() []string {
